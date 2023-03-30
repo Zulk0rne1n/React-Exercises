ea13da0b1e3cf0cc3a59712a316fa0f855664a1c
ulas <ulasardaburak@outlook.com>
2023-03-30 23:10:23 +0300

    Conditional Rendering - 03 added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { Hello } from "./Hello"
 import { Welcome } from "./Welcome"
-import { Age } from "./Age"
+
+
 export class App extends React.Component {
     render() {
         return (
