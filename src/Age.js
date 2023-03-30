9f1e8247b79f42703687b093ba8228285f2cc5d6
ulas <ulasardaburak@outlook.com>
2023-03-30 23:39:12 +0300

    Conditional Rendering - 05 added

--- src/Age.js
+++ src/Age.js
@@ -4,7 +4,11 @@ import { Welcome } from "./Welcome";
 
 export class Age extends React.Component {
     render() {
-        return <p>Your age is {this.props.age}</p>
+        return (
+            this.props.age >= 18 ? 
+            <p>Your age is {this.props.age}</p> :
+            <p>You are too young!</p>
+        )
         
     }
 }
