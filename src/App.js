525dd556d4858ce4f29518e33dcfd414b1944ffe
ulas <ulasardaburak@outlook.com>
2023-03-30 23:13:30 +0300

    Conditional Rendering 04 added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export class App extends React.Component {
         return (
             <div>
                 <Hello />
-                <Welcome name={<strong>Ulas</strong>} age={29} />
+                <Welcome name="John" age={29} />
             </div>)
     }
 }
