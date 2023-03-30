e4ffaa14a439697603ebab7971aa05519924c4e0
ulas <ulasardaburak@outlook.com>
2023-03-30 15:36:20 +0300

    props-05 added

---
+++ src/Age.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Welcome } from "./Welcome";
+
+
+export class Age extends React.Component {
+    render() {
+        return <p>Your age is {this.props.age}</p>
+        
+    }
+}
--- src/Welcome.js
+++ src/Welcome.js
@@ -5,8 +5,7 @@ export class Welcome extends React.Component {
     render() {
         return (
             <div>
-                <p>Welcome {this.props.name}</p>
-                <p>Your age is {this.props.age}</p>
+                <p>Welcome {this.props.name}</p> 
             </div>
         )
     }
