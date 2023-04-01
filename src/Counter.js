fb7dfa581bc076d2417f4932e65ef05f230c34a6
ulas <ulasardaburak@outlook.com>
2023-04-01 03:54:54 +0300

    State 01 added

---
+++ src/Counter.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+export class Counter extends React.Component {
+    state = {
+        count: this.props.initialValue,
+    }
+
+    constructor(props) {
+        super(props)
+
+        setInterval(() => {
+            this.setState((state) => {
+                return {
+                    count: state.count + 1
+                }
+            })
+        }, 1000);
+    }
+
+    render() {
+        return <div>
+            <h1>Count: {this.state.count}</h1>
+        </div>
+    }
+}
+
+Counter.defaultProps = {
+    initialValue: 0,
+
+}
