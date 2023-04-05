17690f61c711a44f70b0d758511871780cad99b9
ulas <ulasardaburak@outlook.com>
2023-04-05 17:33:41 +0300

    Events 01 added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Counter } from "./Counter"
 import { Hello } from "./Hello"
 import { Welcome } from "./Welcome"
+import { ClickCounter } from "./ClickCounter"
 
 
 export class App extends React.Component {
@@ -11,6 +12,7 @@ export class App extends React.Component {
                 <Hello />
                 <Welcome name="John" age={20} />
                 <Counter incrementAmount={1} timeout={500} initialValue={20}/>
+                <ClickCounter initialValue={0} incrementAmount={3}/>
             </div>)
     }
 }
