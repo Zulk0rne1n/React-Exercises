bf12a66cb6cb911d4713c85224c985f62d0650f0
ulas <ulasardaburak@outlook.com>
2023-04-09 02:56:01 +0300

    Lists 05 added

--- src/TodoList.js
+++ src/TodoList.js
@@ -19,13 +19,21 @@ export class TodoList extends React.Component {
         this._inputRef.current.value = ""
     }
 
+    handleReset = () => {
+        this.setState({
+            items: []
+        })
+    }
+
     render() {
         console.log(this.state.items);
+
         return (
             <div>
                 <h3>Todo List</h3>
                 <input ref={this._inputRef} type="text"></input>
                 <button type='submit' onClick={this.handleSubmit}>Submit</button>
+                <button onClick={this.handleReset} >Reset</button>
                 <ul>
                     {this.state.items.map((item, index) => (
                         <li key={item + index}>{item}</li>
