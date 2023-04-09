89a20c3a3596af33f8838455100a8df52995c4b0
ulas <ulasardaburak@outlook.com>
2023-04-09 16:55:09 +0300

    Styling 03 added

--- src/Login.js
+++ src/Login.js
@@ -29,13 +29,16 @@ export class Login extends React.Component {
 
     render() {
         const isDisabled = !this.state.username || !this.state.password
+        const LoginStyle = {
+            backgroundColor: this.state.password.length < 8 ? 'red' : 'green'
+        }
 
         return (
             <div>
                 <input name="username" type="text" value={this.state.username} onChange={this.handleInputChange} />
                 <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange} />
                 <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handleInputChange} />
-                <button onClick={this.handleLogin} disabled={isDisabled}>login</button>
+                <button style={LoginStyle} onClick={this.handleLogin} disabled={isDisabled}>login</button>
             </div>
         )
     }
