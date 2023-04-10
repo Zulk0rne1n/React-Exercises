83afb18f7dd4f9b7d11ed9abc69c3e47f8330917
ulas <ulasardaburak@outlook.com>
2023-04-10 20:29:03 +0300

    Function Components 02 added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import Sum from "./Sum"
 export class App extends React.Component {
 
     render() {
-        const numArr = [0, 5, 13, 29, 56]
 
         return (
             <div>
-                <Sum numbers={numArr} />
+                <Sum />
             </div>)
     }
 }
--- src/Sum.js
+++ src/Sum.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Sum(props) {
-    
-    const sum = props.numbers.reduce((total, num) => total + num, 0);
+function Sum({ numbers = [0, 1, 2, 3, 4] }) {
+
+    const sum = numbers.reduce((total, num) => total + num, 0);
     return <h1>The sum of the numbers is: {sum}</h1>;
 }
 
