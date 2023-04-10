0b1795c86f5df28746157b34aaa7f1ab340a0d20
ulas <ulasardaburak@outlook.com>
2023-04-10 22:12:59 +0300

    useEffect 02 added

--- src/Counter.js
+++ src/Counter.js
@@ -1,31 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
+export function Counter(props) {
+    const [count, setCount] = useState(props.initialValue);
 
-export class Counter extends React.Component {
-    state = {
-        count: this.props.initialValue,
-    }
-
-    constructor(props) {
-        super(props)
-
-        setInterval(() => {
-            this.setState((state) => {
-                return {
-                    count: state.count + 1
-                }
-            })
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCount(count => count + 1);
         }, 1000);
-    }
 
-    render() {
-        return <div>
-            <h1>Count: {this.state.count}</h1>
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <div>
+            <h1>Count: {count}</h1>
         </div>
-    }
+    );
 }
 
-Counter.defaultProps = {
-    initialValue: 0,
 
-}
+
+
+
+// export class Counter extends React.Component {
+//     state = {
+//         count: this.props.initialValue,
+//     }
+
+//     constructor(props) {
+//         super(props)
+
+//         setInterval(() => {
+//             this.setState((state) => {
+//                 return {
+//                     count: state.count + 1
+//                 }
+//             })
+//         }, 1000);
+//     }
+
+//     render() {
+//         return <div>
+//             <h1>Count: {this.state.count}</h1>
+//         </div>
+//     }
+// }
+
+// Counter.defaultProps = {
+//     initialValue: 0,
+
+// }
