aff63780750437c70872cf7f3058fc9a6c73d107
ulas <ulasardaburak@outlook.com>
2023-04-26 13:38:03 +0300

    router 02 added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from './Welcome'
+import { Counter } from "./Counter";
 
 
 export function App() {
@@ -7,6 +8,7 @@ export function App() {
         <div>
             <Routes>
                 <Route path="/" element={<Welcome />} />
+                <Route path="/counter" element={<Counter />} />
             </Routes>
         </div>
     )
