da202e3c53e9808e602db31de685bfed2ab0c02a
ulas <ulasardaburak@outlook.com>
2023-05-22 12:09:45 +0300

    swr 01 added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
-import { Link, Route, Routes } from "react-router-dom";
-import { Welcome } from './Welcome'
-import { Counter } from "./Counter";
 import { GithubUser } from "./GithubUser";
-import { ShowGithubUser } from "./ShowGithubUser";
-
-
 export function App() {
-    return (
-        <div>
-            <Routes>
-                <Route path="/" element={<Welcome />} />
-                <Route path="/counter" element={<Counter />} />
-                <Route path='/users' element={
-                    <div>
-                        <h3>Add a user and select it</h3>
-                        <ShowGithubUser />
-                    </div>
-                } >
-                    <Route path='/users/:user' element={<GithubUser />}> </Route>
-                </Route>
-                <Route path='*' element={<h3>Not Found</h3>} />
-            </Routes>
-            <div>
-                <Link to='/counter'>Counter</Link>
-            </div>
-            <div>
-                <Link to='/users/'>GitHubUser</Link>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <GithubUser />
+    </div>
+  )
 }
--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,36 +1,65 @@
-import { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useGithubUser } from "./useGithubUser";
+import { useState } from "react";
 
+export function GithubUser() {
+    const [usernameInput, setUsernameInput] = useState("");
+    const { data, error, loading, onFetch } = useGithubUser(usernameInput);
 
-export function GithubUser({ userX }) {
-    const [data, setData] = useState(null)
-    const navigate = useNavigate()
-    const { user } = useParams();
-
-
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${userX}`)
-            .then(response => {
-                return response.json()
-            })
-            .then(json => {
-                console.log(json);
-
-                setData(json)
-            })
-    }, [userX])
-
-
+    function onInputChange(event) {
+        setUsernameInput(event.target.value)
+    }
 
     return (
         <div>
+            <hr />
+            <h1>Github Username Fetcher!</h1>
+            <input type="text" name="usernameInput" placeholder="username" value={usernameInput} onChange={onInputChange} />
+            <button onClick={() => onFetch(usernameInput)}>Fetch</button>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error.message}</p>}
             {data && <div>
                 <h2>Username: {data.login}</h2>
                 <h1>Full Name: {data.name}</h1>
                 <img style={{ width: '150px', height: '150px' }} src={data.avatar_url}></img>
             </div>}
-
-            <Link to='/'>Back</Link>
+            <hr />
         </div>
-    )
+    );
 }
+
+// import { useEffect, useState } from "react";
+// import { Link, useNavigate, useParams } from "react-router-dom";
+
+
+// export function GithubUser({ userX }) {
+//     const [data, setData] = useState(null)
+//     const navigate = useNavigate()
+//     const { user } = useParams();
+
+
+//     useEffect(() => {
+//         fetch(`https://api.github.com/users/${userX}`)
+//             .then(response => {
+//                 return response.json()
+//             })
+//             .then(json => {
+//                 console.log(json);
+
+//                 setData(json)
+//             })
+//     }, [userX])
+
+
+
+//     return (
+//         <div>
+//             {data && <div>
+//                 <h2>Username: {data.login}</h2>
+//                 <h1>Full Name: {data.name}</h1>
+//                 <img style={{ width: '150px', height: '150px' }} src={data.avatar_url}></img>
+//             </div>}
+
+//             <Link to='/'>Back</Link>
+//         </div>
+//     )
+// }
