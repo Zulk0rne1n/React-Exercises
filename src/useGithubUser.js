4b8ca92c9e08267db4a24780a703ab4532cb4eb5
ulas <ulasardaburak@outlook.com>
2023-05-22 12:11:17 +0300

    swr 02 added

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -4,18 +4,18 @@ import useSWR from "swr";
 const fetcher = url => fetch(url).then(response => response.json())
 
 export function useGithubUser() {
-    const [usernameInput, setUsernameInput] = useState("");
-    const { data, error, mutate } = useSWR(`https://api.github.com/users/${usernameInput}`, fetcher);
+  const [usernameInput, setUsernameInput] = useState("");
+  const { data, error, mutate } = useSWR(usernameInput ? `https://api.github.com/users/${usernameInput}` : null, fetcher);
+  
+  function onMutate(username) {
+    setUsernameInput(username)
+    mutate()
+  }
 
-    function onMutate(username) {
-        setUsernameInput(username)
-        mutate()
-    }
-
-    return {
-        data: data,
-        error: error,
-        loading: !error && !data,
-        onFetch: onMutate,
-    };
+  return {
+    data: data,
+    error: error,
+    loading: !error && !data,
+    onFetch: onMutate,
+  };
 }
